417d76c2075a0c7ddcbd741d9881fd4dda8457ac
fede sandoval <federico.sandoval@alu.unq.edu.ar>
2024-12-18 16:45:13 -0300

    :white_check_mark: eliminar atributos y focus al crear
    Co-authored-by: Facundo Javier Gelatti <javiergelatti@gmail.com>

--- src/vista.ts
+++ src/vista.ts
@@ -1,24 +1,35 @@
 import {Entidad} from "./entidad.ts";
 import {hacerArrastrable} from "./arrastrable.ts";
-import {point} from "./posicion.ts";
-
-function vistaRepresentandoAtributo(entidad: Entidad, idAtributo: number) {
-    const atributoNuevo = document.createElement("div");
-    atributoNuevo.className = "atributo";
-    const campoNombre = document.createElement("input");
-    campoNombre.value = entidad.nombreAtributo(idAtributo);
-    campoNombre.addEventListener("input", () => {
-        entidad.renombrarAtributo(idAtributo, campoNombre.value);
-    });
-    atributoNuevo.append(campoNombre);
-    return atributoNuevo;
-}
+import {coordenada} from "./posicion.ts";
 
 function posicionarElemento(elementoDOMEntidad: HTMLDivElement, entidad: Entidad) {
     elementoDOMEntidad.style.translate = `${entidad.posicion().x}px ${entidad.posicion().y}px`;
 }
 
-function vistaRepresentandoEntidad(entidad: Entidad) {
+function agrearAtributoEn(contenedorAtributos: HTMLDivElement, entidad: Entidad, idAtributo: number) {
+    const atributoNuevo = document.createElement("div");
+    atributoNuevo.className = "atributo";
+    const campoNombre = document.createElement("input");
+    campoNombre.value = entidad.nombreAtributo(idAtributo);
+    campoNombre.title = "Nombre de atributo";
+    campoNombre.addEventListener("input", () => {
+        entidad.renombrarAtributo(idAtributo, campoNombre.value);
+    });
+    atributoNuevo.append(campoNombre);
+
+    atributoNuevo.addEventListener("click", (evento) => {
+        if (evento.ctrlKey && evento.shiftKey) {
+            evento.stopPropagation();
+            atributoNuevo.remove();
+            entidad.atributos().splice(idAtributo, 1);
+            console.log(`Atributo eliminado`);
+        }
+    });
+    contenedorAtributos.append(atributoNuevo);
+    campoNombre.focus();
+}
+
+function vistaRepresentandoEntidad(entidad: Entidad, entidadesEnModelo: Entidad[]) {
 
     const elementoDOMEntidad = document.createElement("div");
     elementoDOMEntidad.className = "entidad";
@@ -30,17 +41,18 @@ function vistaRepresentandoEntidad(entidad: Entidad) {
         entidad.cambiarNombre(campoNombre.value);
     });
     const contenedorAtributos = document.createElement("div");
-    const botonAgregarPropiedad = document.createElement("button");
-    botonAgregarPropiedad.textContent = "+";
-    botonAgregarPropiedad.addEventListener("click", () => {
+    const botonAgregarAtributo = document.createElement("button");
+    botonAgregarAtributo.textContent = "+";
+    botonAgregarAtributo.title = "Agregar atributo";
+    botonAgregarAtributo.addEventListener("click", () => {
         const idAtributo = entidad.agregarAtributo("");
-        contenedorAtributos.append(vistaRepresentandoAtributo(entidad, idAtributo));
+        agrearAtributoEn(contenedorAtributos, entidad, idAtributo);
     });
 
     entidad.atributos().forEach((_, indice) => {
-        contenedorAtributos.append(vistaRepresentandoAtributo(entidad, indice));
+        agrearAtributoEn(contenedorAtributos, entidad, indice);
     })
-    elementoDOMEntidad.append(campoNombre, botonAgregarPropiedad, contenedorAtributos);
+    elementoDOMEntidad.append(campoNombre, botonAgregarAtributo, contenedorAtributos);
 
     hacerArrastrable(elementoDOMEntidad, {
         alAgarrar: () => {
@@ -54,23 +66,33 @@ function vistaRepresentandoEntidad(entidad: Entidad) {
         },
         alSoltar: () => elementoDOMEntidad.classList.remove("moviendose"),
     });
+    requestAnimationFrame(() => campoNombre.focus());
+
+    elementoDOMEntidad.addEventListener("click", (evento) => {
+        if (evento.ctrlKey && evento.shiftKey) {
+            elementoDOMEntidad.remove(); // Eliminar del DOM
+            entidadesEnModelo.splice(entidadesEnModelo.indexOf(entidad));
+            console.log(`Entidad eliminada: ${entidad.nombre()}`);
+        }
+    });
 
     return elementoDOMEntidad;
 }
 
-export function init(elementoRaiz: HTMLElement, entidadesIniciales: [Entidad]) {
+export function init(elementoRaiz: HTMLElement, entidadesEnModelo: Entidad[]) {
     elementoRaiz.addEventListener("dblclick", evento => {
         if (evento.target !== elementoRaiz) return;
 
-        const posicion = point(evento.offsetX, evento.offsetY);
+        const posicion = coordenada(evento.offsetX, evento.offsetY);
         const entidad = new Entidad("", [], posicion);
+        entidadesEnModelo.push(entidad);
 
-        elementoRaiz.append(vistaRepresentandoEntidad(entidad));
+        elementoRaiz.append(vistaRepresentandoEntidad(entidad, entidadesEnModelo));
 
         console.log(entidad);
     });
 
-    const [entidad] = entidadesIniciales;
-
-    elementoRaiz.append(vistaRepresentandoEntidad(entidad));
+    entidadesEnModelo.forEach(entidad => {
+        elementoRaiz.append(vistaRepresentandoEntidad(entidad, entidadesEnModelo));
+    });
 }
--- test/browser/front.test.ts
+++ test/browser/front.test.ts
@@ -1,8 +1,8 @@
 import {beforeEach, describe, expect, it} from "vitest";
-import {within, fireEvent} from "@testing-library/dom";
+import {fireEvent, within} from "@testing-library/dom";
 import {init} from "../../src/vista";
 import {Entidad} from "../../src/entidad";
-import {point} from "../../src/posicion";
+import {coordenada} from "../../src/posicion";
 import "../../src/style.css";
 
 function getElementoEntidades() {
@@ -15,20 +15,37 @@ function campoNombreDe(elementoEntidad: HTMLElement) {
 
 function cambiarNombreEntidadPor(elementoEntidad: HTMLElement, nuevoValor: string) {
     const campoNombre = campoNombreDe(elementoEntidad);
-
     fireEvent.input(campoNombre, {target: {value: nuevoValor}});
+}
+
+function realizarGestoEliminarSobre(elemento: HTMLElement) {
+    fireEvent.click(elemento, {ctrlKey: true, shiftKey: true});
+}
+
+function agregarAtributoEn(elementoEntidad: HTMLElement, nombreAtributoNuevo: string) {
+    const botonAgregarAtributo = within(elementoEntidad).getByTitle<HTMLButtonElement>("Agregar atributo");
+
+    botonAgregarAtributo.click();
+
+    const campoAtributo = within(elementoEntidad).getByTitle<HTMLInputElement>("Nombre de atributo");
+    fireEvent.input(campoAtributo, {target: {value: nombreAtributoNuevo}});
 }
 
 describe("[MER] Vista Modelo tests", () => {
     let entidad: Entidad;
+    let elementoRaiz: HTMLElement;
+    let entidadesEnModelo: Entidad[];
 
     beforeEach(() => {
         document.body.innerHTML = '';
-        entidad = new Entidad("Pirata", [], point(10, 10));
-        init(document.body, [entidad]);
-    })
+        elementoRaiz = document.createElement('div');
+        document.body.append(elementoRaiz);
+        entidad = new Entidad("Pirata", [], coordenada(10, 10));
+        entidadesEnModelo = [entidad];
+        init(elementoRaiz, entidadesEnModelo);
+    });
 
-    it("[test01] Dada una inicializacion con una entidad, entonces la misma se encuentra en el DOM", () => {
+    it("Dada una inicializacion con una entidad, entonces la misma se encuentra en el DOM", () => {
         const elementoEntidades = getElementoEntidades();
         expect(elementoEntidades.length).toEqual(1);
 
@@ -36,11 +53,67 @@ describe("[MER] Vista Modelo tests", () => {
         expect(campoNombreDe(elementoEntidad).value).toEqual("Pirata");
     });
 
-    it('[test02] Dada una entidad, cuando su nombre es cambiado desde la vista, entonces el cambio se refleja en el modelo', () => {
+    it('Dada una entidad, cuando su nombre es cambiado desde la vista, entonces el cambio se refleja en el modelo', () => {
         const [elementoEntidad] = getElementoEntidades();
 
         cambiarNombreEntidadPor(elementoEntidad, "Marinero");
 
         expect(entidad.nombre()).toEqual("Marinero");
+    });
+
+    it("Cuando se escribe un nombre en la entidad creada, el modelo se actualiza correctamente", async () => {
+        fireEvent.dblClick(elementoRaiz, { clientX: 100, clientY: 100 });
+
+        const elementoEntidades = getElementoEntidades();
+        expect(elementoEntidades.length).toBe(2);
+
+        const nuevaEntidad = elementoEntidades[1];
+        const campoNombre = campoNombreDe(nuevaEntidad);
+
+        fireEvent.input(campoNombre, { target: { value: "Capitán" } });
+
+
+        expect(campoNombre.value).toBe("Capitán");
+    });
+
+    it("Se puede eliminar entidades del MER", () => {
+        const elementoEntidades = getElementoEntidades();
+        expect(elementoEntidades.length).toBe(1);
+
+        const [elementoEntidad] = elementoEntidades;
+
+        realizarGestoEliminarSobre(elementoEntidad);
+
+        expect(getElementoEntidades().length).toBe(0);
+        expect(entidadesEnModelo.length).toBe(0);
+    });
+
+    it("Se puede agregar un atributo a una entidad existente", () => {
+        const [elementoEntidad] = getElementoEntidades();
+
+        agregarAtributoEn(elementoEntidad, "nombre");
+
+        expect(entidad.atributos()).toEqual(["nombre"]);
+    });
+
+    it("Cuando se agrega un atributo, se enfoca el campo para nombrarlo", () => {
+        const [elementoEntidad] = getElementoEntidades();
+
+        const botonAgregarAtributo = within(elementoEntidad).getByTitle<HTMLButtonElement>("Agregar atributo");
+        botonAgregarAtributo.click();
+
+        const [campoAtributo] = within(elementoEntidad).getAllByTitle<HTMLInputElement>("Nombre de atributo");
+        expect(document.activeElement).toEqual(campoAtributo);
+    });
+
+    it("Cuando se elimina un atributo, este deja de existir en el MER", () => {
+        const [elementoEntidad] = getElementoEntidades();
+        agregarAtributoEn(elementoEntidad, "nombre");
+
+        const [campoAtributo] = within(elementoEntidad).getAllByTitle<HTMLInputElement>("Nombre de atributo");
+
+        realizarGestoEliminarSobre(campoAtributo);
+
+        expect(entidad.atributos()).toHaveLength(0);
     });
 });
