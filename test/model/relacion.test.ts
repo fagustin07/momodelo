259df0daaafeadf2b903751b81312804033467fd
fagustin07 <federico.sandoval@alu.unq.edu.ar>
2025-04-09 05:04:57 -0300

    :white_check_mark: ajustes de test de relacion por modelo simplificado

--- test/model/relacion.test.ts
+++ test/model/relacion.test.ts
@@ -1,14 +1,14 @@
 import { describe, it, expect } from "vitest";
 import { Entidad } from "../../src/modelo/entidad.ts";
-import { Maxima, Minima, Relacion, Rol } from "../../src/modelo/relacion.ts";
 import { coordenada } from "../../src/posicion.ts";
+import {Relacion} from "../../src/modelo/relacion.ts";
 
 describe("[MER] Relación", () => {
     it("Dado dos entidades, cuando se crea una relación, entonces debe conocer su nombre y las entidades asociadas", () => {
         const entidad1 = new Entidad("Pirata", [], coordenada(0, 0));
         const entidad2 = new Entidad("Marin", [], coordenada(100, 100));
 
-        const relacion = new Relacion("Combate", entidad1, entidad2);
+        const relacion = new Relacion("Combate", entidad1, entidad2, coordenada(50, 50));
 
         expect(relacion.nombre()).toEqual("Combate");
         expect(relacion.entidades()).toEqual([entidad1, entidad2]);
@@ -17,22 +17,10 @@ describe("[MER] Relación", () => {
     it("Dada una relacion, cuando se modifica su nombre, entonces se refleja correctamente", () => {
         const entidad1 = new Entidad("Persona", [], coordenada(0, 0));
         const entidad2 = new Entidad("Fruta", [], coordenada(100, 100));
-        const relacion = new Relacion("Consume", entidad1, entidad2);
+        const relacion = new Relacion("Consume", entidad1, entidad2, coordenada(50, 50));
 
-        relacion.setNombre("Come")
+        relacion.cambiarNombre("Come")
 
         expect(relacion.nombre()).toEqual("Come");
-    });
-
-    it("Dada una relación, cuando se eligen las cardinalidades, entonces quedan bien representadas", () => {
-        const entidad1 = new Entidad("Luffy", [], coordenada(0, 0));
-        const entidad2 = new Entidad("Zoro", [], coordenada(100, 100));
-
-        const relacion = new Relacion("Tripulación", entidad1, entidad2);
-        relacion.setCardinalidad(Rol.ORIGEN, [Minima.UNO, Maxima.N]);
-        relacion.setCardinalidad(Rol.DESTINO, [Minima.CERO, Maxima.UNO]);
-
-        expect(relacion.getCardinalidad(Rol.ORIGEN)).toEqual([Minima.UNO, Maxima.N]);
-        expect(relacion.getCardinalidad(Rol.DESTINO)).toEqual([Minima.CERO, Maxima.UNO]);
     });
 });
